4ae9f15cd263bab234af1e8e71cfd314d7a5075b
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-08-22 20:46:04 -0300

    Feat: adding post and new array

---
+++ src/Components/Post/PostCard.jsx
@@ -0,0 +1,28 @@
+import { FaRegComment, FaRegHeart } from "react-icons/fa";
+
+
+export default function PostCard({ post }) {
+  return (
+    <div className="p-4 mb-4">
+      <div className="flex items-center mb-2 gap-2">
+        <img src={post.profilePhoto} alt={post.profileName} className="w-10 h-10 rounded-full" />
+        <h2 className="text-xl font-bold mb-2">{post.profileName}</h2>
+      </div>
+      <div className="font-semibold">{post.title}</div>
+      <p>{post.content}</p>
+      {post.tags.map((tag, index) => (
+          <span key={index} className="font-semibold text-blue-500">
+            #{tag} {' '}
+          </span>
+        ))}
+      <div className="m-2 flex gap-4">
+        <span className="cursor-pointer">
+          <FaRegHeart />
+        </span>
+        <span className="cursor-pointer">
+          <FaRegComment />
+        </span>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,25 @@
+import PostCard from "../../Components/Post/PostCard";
 import ProfileInfoCard from "../../Components/Profile/ProfileInfoCard";
 import Container from "../../Components/UX/Container/Container";
 
-// Defina uma lista de postagens
 const posts = [
-  { id: 1, title: "Postagem 1", content: "Conteúdo da postagem 1" },
-  { id: 2, title: "Postagem 2", content: "Conteúdo da postagem 2" },
-  { id: 3, title: "Postagem 3", content: "Conteúdo da postagem 3" },
-  { id: 4, title: "Postagem 4", content: "Conteúdo da postagem 4" },
-  { id: 1, title: "Postagem 1", content: "Conteúdo da postagem 1" },
-  { id: 2, title: "Postagem 2", content: "Conteúdo da postagem 2" },
-  { id: 3, title: "Postagem 3", content: "Conteúdo da postagem 3" },
-  { id: 4, title: "Postagem 4", content: "Conteúdo da postagem 4" },
-  // Adicione mais postagens conforme necessário
+  { id: 1, profileName: "João Inspirador", profilePhoto: "https://via.placeholder.com/150", title: "Acredite em Si Mesmo", content: "Acredite em si mesmo e tudo será possível.", tags: ["autoestima", "confiança", "motivação"], date: "2024-08-22T08:00:00Z" },
+  { id: 2, profileName: "Maria Motivadora", profilePhoto: "https://via.placeholder.com/150", title: "Esforço Contínuo", content: "O sucesso é a soma de pequenos esforços repetidos diariamente.", tags: ["disciplina", "consistência", "sucesso"], date: "2024-08-22T09:30:00Z" },
+  { id: 3, profileName: "Carlos Determinado", profilePhoto: "https://via.placeholder.com/150", title: "Primeiro Passo", content: "A jornada de mil milhas começa com um único passo.", tags: ["ação", "iniciativa", "crescimento"], date: "2024-08-22T10:45:00Z" },
+  { id: 4, profileName: "Ana Conquistadora", profilePhoto: "https://via.placeholder.com/150", title: "Nunca é Tarde", content: "Nunca é tarde para ser o que você poderia ter sido.", tags: ["oportunidade", "mudança", "determinação"], date: "2024-08-22T11:15:00Z" },
+  { id: 5, profileName: "Felipe Visionário", profilePhoto: "https://via.placeholder.com/150", title: "Olhos no Objetivo", content: "Os obstáculos são aquelas coisas assustadoras que você vê quando desvia os olhos do seu objetivo.", tags: ["foco", "superação", "objetivos"], date: "2024-08-22T13:00:00Z" },
+  { id: 6, profileName: "Clara Otimista", profilePhoto: "https://via.placeholder.com/150", title: "Melhorar o Amanhã", content: "O que você faz hoje pode melhorar todos os seus amanhãs.", tags: ["planejamento", "futuro", "desenvolvimento"], date: "2024-08-22T14:30:00Z" },
+  { id: 7, profileName: "Lucas Persistente", profilePhoto: "https://via.placeholder.com/150", title: "Caminho para o Êxito", content: "A persistência é o caminho do êxito.", tags: ["persistência", "sucesso", "motivação"], date: "2024-08-22T15:00:00Z" },
+  { id: 8, profileName: "Sofia Inspiradora", profilePhoto: "https://via.placeholder.com/150", title: "Foco na Jornada", content: "A felicidade não é o destino, é a jornada.", tags: ["felicidade", "jornada", "vida"], date: "2024-08-22T16:00:00Z" },
+  { id: 9, profileName: "Ricardo Resiliente", profilePhoto: "https://via.placeholder.com/150", title: "Transforme Dificuldades em Oportunidades", content: "As dificuldades são oportunidades disfarçadas.", tags: ["resiliência", "oportunidade", "superação"], date: "2024-08-22T17:00:00Z" },
+  { id: 10, profileName: "Beatriz Corajosa", profilePhoto: "https://via.placeholder.com/150", title: "Saia da Zona de Conforto", content: "Tudo o que você deseja está do outro lado do medo.", tags: ["coragem", "zona de conforto", "mudança"], date: "2024-08-22T18:00:00Z" },
+  { id: 11, profileName: "Pedro Decidido", profilePhoto: "https://via.placeholder.com/150", title: "Ação Imediata", content: "Não espere o momento perfeito, faça o momento perfeito.", tags: ["ação", "imediato", "proatividade"], date: "2024-08-22T19:00:00Z" },
+  { id: 12, profileName: "Laura Forte", profilePhoto: "https://via.placeholder.com/150", title: "Seja Resiliente", content: "A vida não fica mais fácil, você fica mais forte.", tags: ["resiliência", "força", "vida"], date: "2024-08-22T20:00:00Z" },
+  { id: 13, profileName: "André Confiante", profilePhoto: "https://via.placeholder.com/150", title: "Autoconfiança é a Chave", content: "Confie em si mesmo. Você sabe mais do que imagina.", tags: ["autoconfiança", "autoestima", "sucesso"], date: "2024-08-22T21:00:00Z" },
+  { id: 14, profileName: "Luiza Vitoriosa", profilePhoto: "https://via.placeholder.com/150", title: "Vença a Si Mesmo", content: "O único limite para o seu sucesso é o que você acredita ser possível.", tags: ["sucesso", "autoestima", "superação"], date: "2024-08-22T22:00:00Z" },
+  { id: 15, profileName: "Marcos Determinado", profilePhoto: "https://via.placeholder.com/150", title: "Determinação Diária", content: "Pequenos passos na direção certa são o que constroem grandes vitórias.", tags: ["determinação", "vitória", "passos"], date: "2024-08-22T23:00:00Z" },
 ];
+
 
 export default function Home() {
   
@@ -23,21 +30,21 @@ export default function Home() {
       <p className="text-gray-700 mb-4">
         Comece seu dia com motivação e inspiração. Explore as postagens e encontre sua dose diária de positividade.
       </p>
-      <div className="flex gap-4">
+      <div className="flex flex-1 gap-4 ">
         <div className="w-[15rem] flex flex-col h-[calc(100vh-15rem)]">
           <ProfileInfoCard/>
           
         </div>
-        <div className="flex-1 h-[calc(100vh-15rem)] border overflow-scroll ">
-        <ul>
-            {posts.map(post => (
-              <li key={post.id} className={"border p-4 mb-4"}>
-                <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
-                <p>{post.content}</p>
-              </li>
-            ))}
-          </ul>
+        <div style={{
+          scrollbarWidth: 'none'
+        }} className=" w-full h-[calc(100vh-15rem)] overflow-scroll ">
+          <div className="bg-slate-50">
+              {posts.map(post => (
+              <PostCard  key={post.id} post={post}  />
+              ))}
+          </div>
         </div>
+        <div className="w-96">teste</div>
       </div>
     </Container>
   );
