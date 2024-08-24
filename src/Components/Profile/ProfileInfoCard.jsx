9f7aee949557bdef042c79a7b83d17f4a3352668
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-08-23 21:04:17 -0300

    Feat: add img

--- src/Components/Profile/ProfileInfoCard.jsx
+++ src/Components/Profile/ProfileInfoCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Button from '../UX/Button/Button';
+import jose from './jose.png'
 
 export default function ProfileInfoCard () {
   return (
     <div className="p-4 rounded text-gray-500">
-      <img src="https://via.placeholder.com/150" alt="Profile" className="w-32 h-32 rounded-full mx-auto mb-4" />
+      <img src={jose} alt="Profile" className="w-32 h-32 rounded-full mx-auto mb-4" />
       <h2 className='text-center font-bold'>José Erisvaldo</h2>
       <div className='flex flex-col justify-center items-center gap-4'>
         <div className='flex justify-center items-center gap-4'>
