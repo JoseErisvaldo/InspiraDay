fba01dcd13af07d1fc1a09c581a7c7ac2604a092
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-08-24 21:18:49 -0300

    styles:  bg-blue

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-blue-600 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-2xl font-bold">
           InspiraDay
