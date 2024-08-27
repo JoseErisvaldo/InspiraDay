a9f5145838e1a315664b8a27e3df2561f79657d5
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-08-26 22:04:59 -0300

    Feat: Input

---
+++ src/Components/UX/Input/Input.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Input({
+  type,
+  name,
+  value,
+  onChange,
+  list,
+  placeholder,
+}) {
+  return (
+    <input
+      placeholder={placeholder}
+      type={type}
+      name={name}
+      value={value}
+      onChange={onChange}
+      list={list}
+      className="mt-1 block w-full rounded-md p-2 border "
+      required
+    />
+  )
+}
